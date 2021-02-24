1f3c3d3f2bf5b24249f010b23f7dfa5e95e5d878
basedmilz <basedmilz@gmail.com>
2021-02-24 01:05:27 -0500

    restarting

--- javascript.js
+++ javascript.js
@@ -2,30 +2,3 @@ var apiKey = '4d52cf6d12c63448e460f11ea75544d2';
 var units = 'imperial';
 var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey
 var currencity = '';
-var finder = document.querySelector("finder")
-var displayer = document.querySelector("thecity")
-
-fetch(cityUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-
-console.log(finder)
-
-var currentUpdate = function (x) {
-    fetch(cityUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            displayWeather(data, x);
-        })
-}
-
-var displayWeather = function(finder) {
-    displayer.textContent = finder;
-
-}
