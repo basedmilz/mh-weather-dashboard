541f68d8274e211be87bfdcbfa8f4b486738e1dc
basedmilz <basedmilz@gmail.com>
2021-02-24 17:57:12 -0500

    javascript update linked all variable ids

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// Variable IDs
+
 var apiKey = '4d52cf6d12c63448e460f11ea75544d2';
 var units = 'imperial';
 var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 'city' + '&appid=' + apiKey
@@ -5,6 +7,13 @@ var currencity = '';
 currentDate = moment()
 moment().format('MMMM Do YYYY, h:mm:ss a')
 console.log (moment ())
+var locator = $("#cityInput")
+var searchHistory = $("history")
+var submitButton = $("submitCity")
+var currentCity = $("current-city")
+var tempNow = $("temperature")
+var humidity =$("#current-humidity")
+var windspeed = $("#current-wind-speed")
+var uv = $("#uv-index")
+var weatherContent = $("weather-content")
 
-
-var inputText = document.querySelector('#text')
