ab20c07d4beec36896c7fa81868a1892edba2dae
basedmilz <basedmilz@gmail.com>
2021-02-24 01:46:23 -0500

    1:46 update

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 var apiKey = '4d52cf6d12c63448e460f11ea75544d2';
 var units = 'imperial';
-var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey
+var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 'city' + '&appid=' + apiKey
 var currencity = '';
+currentDate = moment()
+moment().format('MMMM Do YYYY, h:mm:ss a')
+console.log (moment ())
