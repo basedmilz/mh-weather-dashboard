a21a35e2c925354a6578532a07c91eb6975fe110
basedmilz <basedmilz@gmail.com>
2021-02-24 21:51:58 -0500

    adjust javascript

--- javascript.js
+++ javascript.js
@@ -5,13 +5,11 @@ var units = 'imperial';
 
 // Variable IDs
 // var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + locator + '&units=imperial&appid=' + apiKey;
-var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + locator + '&appid=' + apiKey + '&units=imperial';        
 // var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Henderson&units=imperial&appid=4d52cf6d12c63448e460f11ea75544d2;
 var currencity = '';
 currentTime = moment();
 $("#currentTime").text(currentTime.format("dddd MMM Do, YYYY h:mm:ss a"));
 
-console.log(cityUrl)
 var locator = $("#city-input")
 var searchHistory = $("history");
 var submitButton = $("submitCity")
@@ -21,6 +19,7 @@ var humidity = $("#current-humidity")
 var windspeed = $("#current-wind-speed")
 var uv = $("#uv-index")
 var weatherContent = $("weather-content")
+var locatorValue = locator.val().trim()
 
 $(document).on('submit', function (event) {
     event.preventDefault();
@@ -28,11 +27,16 @@ $(document).on('submit', function (event) {
 });
 
 
-function apiWeather(){
+function apiWeather(CityID) {
+    var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + locator + '&appid=' + apiKey + '&units=imperial';
     fetch(cityUrl)
-    .then(function(response){
-        return response.json();      
-    })
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+        }).catch(function () {
+
+        })
 
 }
 // fetch(cityUrl)
