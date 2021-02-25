a2ad336cea82592d5aa92cbeabbffc55f4c2002a
basedmilz <basedmilz@gmail.com>
2021-02-24 21:07:36 -0500

    update to html

--- javascript.js
+++ javascript.js
@@ -1,19 +1,46 @@
-// Variable IDs
 
+// My API key to OpenWeather API
 var apiKey = '4d52cf6d12c63448e460f11ea75544d2';
 var units = 'imperial';
-var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + 'city' + '&appid=' + apiKey
+
+// Variable IDs
+// var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + locator + '&units=imperial&appid=' + apiKey;
+var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + locator + '&appid=' + apiKey + '&units=imperial';        
+// var cityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Henderson&units=imperial&appid=4d52cf6d12c63448e460f11ea75544d2;
 var currencity = '';
-currentDate = moment()
-moment().format('MMMM Do YYYY, h:mm:ss a')
-console.log (moment ())
-var locator = $("#cityInput")
-var searchHistory = $("history")
+currentTime = moment();
+$("#currentTime").text(currentTime.format("dddd MMM Do, YYYY h:mm:ss a"));
+
+console.log(cityUrl)
+var locator = $("#city-input")
+var searchHistory = $("history");
 var submitButton = $("submitCity")
 var currentCity = $("current-city")
 var tempNow = $("temperature")
-var humidity =$("#current-humidity")
+var humidity = $("#current-humidity")
 var windspeed = $("#current-wind-speed")
 var uv = $("#uv-index")
 var weatherContent = $("weather-content")
 
+$(document).on('submit', function (event) {
+    event.preventDefault();
+    apiWeather(event);
+});
+
+
+function apiWeather(){
+    fetch(cityUrl)
+    .then(function(response){
+        return response.json();      
+    })
+
+}
+// fetch(cityUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (response) {
+//         currentCity.text(response.name);
+
+//     }
+//     )
